41c492ab622a68f71d0b2f07f59c686f6ff5c31d
Drewthurm21 <drewthurm21@gmail.com>
2022-11-22 11:46:42 -0600

    updated structure for bank starter

--- bank.js
+++ bank.js
@@ -6,43 +6,6 @@
   We'll start by creating a bank class that will store unique info like the
   branch location, branch capital and related accounts.
 
-  Create a bank app that has the following functionality:
-
-  openAccount - (pw, name, initialDeposit)
-    -should create a new account for a customer
-    -account numbers should be assigned sequentially, starting with 0
-    -should add the account funds to total bank capital
-    -should return the new account number
-
-  closeAccount - (pw, acctNum)
-    -should close the intended account for a customer
-    -should remove the funds from bank capital 
-    -customer password and account number are required
-    
-  getAccountInfo - (pw, acctNum)  
-    -should return the account information for a given account number
-
-  checkBalance - (pw, acctNum)
-    -should return the balance of the account
-    
-  processDeposit - (pw, acctNum, amount)
-    -should deposit funds into the customer account
-    -should add the funds to the bank's total capital
-    -should return the newly updated account with all information
-
-  processWithdraw - (pw, acctNum, amount)
-    -should allow a customer to withdrawl from an account
-    -should remove the funds from the account and bank capital if able
-    -should return the newly updated account with all information if successful
-    -should return an error message if the customer does not have enough funds
-    -we do not allow overdrafts
-
-  transferFunds - (pw, acctNum, amount, acctNum2)
-    -should move funds from one account to another, if able
-    -it should return the newly updated account with all information if successful
-    -it should return an error message if the customer does not have enough funds
-    -we do not allow overdrafts
-
 */
 
 
@@ -63,38 +26,66 @@ class Bank {
 
   }
 
-
+  // openAccount - (pw, name, initialDeposit)
+  //   -should create a new account for a customer
+  //   -account numbers should be assigned sequentially, starting with 0
+  //   -should add the account funds to total bank capital
+  //   -should return the new account number
   openAccount(user, password, funds) {
-    const newAccount = {
-      accountNumber: null,
-      userName: null,
-      accountBalance: null,
-      password: null
-    }
+    // example user object
+    // const newAccount = {
+    //   accountNumber: 9453,
+    //   userName: 'John Doe',
+    //   accountBalance: 1076.22,
+    //   password: 'strongPassword'
+    // }
 
     //your code here
   }
 
+  // closeAccount - (pw, acctNum)
+  //   -should close the intended account for a customer
+  //   -should remove the funds from bank capital 
+  //   -customer password and account number are required
   closeAccount(accountNumber, password) {
     //your code here
   }
 
+  // getAccountInfo - (pw, acctNum)  
+  //   -should return the account information for a given account number
   checkAccountInfo(accountNumber, password) {
     //your code here
   }
 
+  // checkBalance - (pw, acctNum)
+  //   -should return the balance of the account
   checkBalance(accountNumber, password) {
     //your code here
   }
 
+  // processDeposit - (pw, acctNum, amount)
+  //   -should deposit funds into the customer account
+  //   -should add the funds to the bank's total capital
+  //   -should return the newly updated account with all information
   processDeposit(accountNumber, password, funds) {
     //your code here
   }
 
+  // processWithdraw - (pw, acctNum, amount)
+  //   -should allow a customer to withdrawl from an account
+  //   -should remove the funds from the account and bank capital if able
+  //   -should return the newly updated account with all information if successful
+  //   -should return an error message if the customer does not have enough funds
+  //   -we do not allow overdrafts
   processWithdrawl(accountNumber, password, funds) {
     //your code here
   }
 
+  // transferFunds - (pw, acctNum, amount, acctNum2)
+  //   -should move funds from one account to another, if able
+  //   -it should return the newly updated account with all information if successful
+  //   -it should return an error message if the customer does not have enough funds
+  //   -we do not allow overdrafts
   transferFunds(accountNumber, password, funds, accountNumber2) {
     //your code here
   }
@@ -119,9 +110,7 @@ class Bank {
 
   //listAccounts should list each account number and balance
   listAccounts() {
-    for (const account of this.accounts) {
-      console.log(account)
-    }
+
   }
 }
 
